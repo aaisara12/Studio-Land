30e91e34cc4b330aeb3035942c4a73e6519fe242
aaisara12 <aisara121201@gmail.com>
2022-09-23 14:52:48 -0700

    Converted black fade screen to UI Document implementation
    Allows for sorting to work and black screen to cover up instruction UI

--- Assets/Scripts/FadeManager.cs
+++ Assets/Scripts/FadeManager.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 using DG.Tweening;
 using UnityEngine.UI;
 using System;
+using UnityEngine.UIElements;
+using UnityEngine.UIElements.Experimental;
 
 public class FadeManager : MonoBehaviour
 {
-    [SerializeField] Image blackScreen;
+    [SerializeField] UIDocument blackScreen;
     [SerializeField] FloatEventChannelSO fadeInRequestChannel;
     [SerializeField] FloatEventChannelSO fadeOutRequestChannel;
 
     void OnEnable()
     {
+        //blackScreen.sortingOrder = 0;
         fadeInRequestChannel.OnEventRaised += HandleFadeInRequested;
         fadeOutRequestChannel.OnEventRaised += HandleFadeOutRequested;
     }
@@ -25,11 +28,17 @@ public class FadeManager : MonoBehaviour
 
     void HandleFadeInRequested(float duration)
     {
-        blackScreen.DOFade(0, duration);
+        //blackScreen.sortingOrder = 0;      // Prevent invisible black screen from blocking raycasts
+        StyleValues newStyle = new StyleValues {opacity = 0};
+        blackScreen.rootVisualElement.Q("Black").experimental.animation.Start(newStyle, (int)(duration * 1000));
+        //blackScreen.DOFade(0, duration);
     }
 
     void HandleFadeOutRequested(float duration)
     {
-        blackScreen.DOFade(1, duration);
+        //blackScreen.sortingOrder = 99;
+        StyleValues newStyle = new StyleValues {opacity = 1};
+        blackScreen.rootVisualElement.Q("Black").experimental.animation.Start(newStyle, (int)(duration * 1000));
+        //blackScreen.DOFade(1, duration);
     }
 }
